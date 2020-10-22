ae5e4e3010ca84b67794fd842a4282caf3fe3469
florianmarsch <dekontaminator@googlemail.com>
2020-10-22 20:51:16 +0200

    add validation for login

--- src/main/kotlin/de/florianmarsch/spring/socialmediacalendar/service/instagram/InstagramPostStrategy.kt
+++ src/main/kotlin/de/florianmarsch/spring/socialmediacalendar/service/instagram/InstagramPostStrategy.kt
@@ -12,9 +12,11 @@ import org.springframework.stereotype.Service
 import java.awt.image.BufferedImage
 import java.io.File
 import java.io.FileOutputStream
+import java.lang.IllegalStateException
 import java.net.URL
 import java.nio.file.Files
 import java.time.LocalDateTime
+import java.time.ZoneId
 import java.util.*
 import javax.imageio.ImageIO
 
@@ -29,45 +31,57 @@ class InstagramPostStrategy : PostingStrategy{
 
         val instagram = Instagram4j.builder().username(channel.username).password(channel.password).build()
 
-        instagram.apply {
-            setup()
-            login()
 
-            val temp: File = File.createTempFile(UUID.randomUUID().toString(),".jpg")
-            URL(posting.picture).openConnection().getInputStream().copyTo(FileOutputStream(temp))
+        val temp: File = File.createTempFile(UUID.randomUUID().toString(),".jpg")
+        URL(posting.picture).openConnection().getInputStream().copyTo(FileOutputStream(temp))
 
 
-            val bimg: BufferedImage = ImageIO.read(temp)
-            val width = bimg.width
-            val height = bimg.height
+        val bimg: BufferedImage = ImageIO.read(temp)
+        val width = bimg.width
+        val height = bimg.height
 
-            if(width != height ){
-                response.status = "Not a square : ${width}x${height} px."
-            }else{
+        if(width != height ){
+            response.status = "Not a square : ${width}x${height} px."
+        }else{
 
 
 
-                val config = TikaConfig.getDefaultConfig()
-                val mimeTypeString :String = Files.probeContentType(temp.toPath())
-                val mimeType: MimeType = config.mimeRepository.forName(mimeTypeString)
-                val extension: String = mimeType.extension
+            val config = TikaConfig.getDefaultConfig()
+            val mimeTypeString :String = Files.probeContentType(temp.toPath())
+            val mimeType: MimeType = config.mimeRepository.forName(mimeTypeString)
+            val extension: String = mimeType.extension
 
-                val file = File.createTempFile(UUID.randomUUID().toString(), ".${extension}")
-                temp.renameTo(file)
+            val file = File.createTempFile(UUID.randomUUID().toString(), ".${extension}")
+            temp.renameTo(file)
 
-                if(posting.text.isNullOrEmpty()){
-                    response.status = "Text is null or empty"
+            if(posting.text.isNullOrEmpty()){
+                response.status = "Text is null or empty"
+            }
+
+            posting.text?.let{
+                try{
+
+                    instagram.apply {
+                        setup()
+                        val login = login()
+                        if(login.isSpam){
+                            throw IllegalStateException("login is spam.")
+                        }
+                        if(login.status == "fail"){
+                            throw IllegalStateException("login.status : ${login.message}.")
+                        }
+
+                        val storyResult: InstagramConfigureMediaResult = instagram.sendRequest(InstagramUploadPhotoRequest(
+                                file, it))
+
+                        response.status = storyResult.status
+                        response.link = storyResult.checkpoint_url
+                        response.publishDate = LocalDateTime.now(ZoneId.of("CET"))
+                    }
+                }catch(e:Throwable){
+                    response.status = e.message
                 }
 
-                posting.text?.let{
-
-                    val storyResult: InstagramConfigureMediaResult = instagram.sendRequest(InstagramUploadPhotoRequest(
-                            file, it))
-
-                    response.status = storyResult.status
-                    response.link = storyResult.checkpoint_url
-                    response.publishDate = LocalDateTime.now()
-                }
             }
         }
 
